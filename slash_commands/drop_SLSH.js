98d24986d116b6f73abfb92f79aefa95753f9ea5
xsqu1zn't <fluffyspiderstudios@gmail.com>
2023-05-26 21:52:36 -1000

    failed fixes

--- slash_commands/drop_SLSH.js
+++ slash_commands/drop_SLSH.js
@@ -1,8 +1,12 @@
-const { Client, CommandInteraction, SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const {
+    Client, CommandInteraction, SlashCommandBuilder, ComponentType,
+    ActionRowBuilder, ButtonBuilder, ButtonStyle
+} = require('discord.js');
 
-const { botSettings: { currencyIcon, customEmojis } } = require('../configs/heejinSettings.json');
-const { BetterEmbed } = require('../modules/discordTools');
+const { botSettings: { currencyIcon, customEmojis, timeout } } = require('../configs/heejinSettings.json');
+const { BetterEmbed, messageTools } = require('../modules/discordTools');
 const { userManager } = require('../modules/mongo');
+const { dateTools } = require('../modules/jsTools');
 const cardManager = require('../modules/cardManager');
 
 module.exports = {
@@ -61,10 +65,10 @@ module.exports = {
                 cards_dropped = cardManager.drop("event2", 1); break;
         }
 
-        /// Add the cards to the user's card_inventory (can cause UIDs to be reset)
-        await userManager.cards.add(interaction.user.id, cards_dropped);
+        // Add the cards to the user's card_inventory (can cause UIDs to be reset)
+        // await userManager.cards.add(interaction.user.id, cards_dropped);
 
-        /// Add details to embed_drop
+        // Add details to embed_drop
         let cards_dropped_f = cards_dropped.map(card => cardManager.toString.inventory(card, { simplify: true }));
 
         // Add a number emoji infront of the card info if there's more than 1 card
@@ -78,15 +82,87 @@ module.exports = {
         embed_drop.setDescription(cards_dropped_f.join("\n"));
         embed_drop.setImage(cards_dropped_last.imageURL);
 
-        /// Add buttons to embed_drop
-        let actionRow_buttons = [
-            new ButtonBuilder().setCustomId("btn_sell").setEmoji(currencyIcon).setStyle(ButtonStyle.Secondary),
-            new ButtonBuilder().setCustomId("btn_vault").setEmoji("🔒").setStyle(ButtonStyle.Secondary)
-        ];
-
-        let actionRow = new ActionRowBuilder().addComponents(actionRow_buttons);
-
         // Send the drop embed
-        await embed_drop.send({ components: actionRow });
+        let message = await embed_drop.send();
+
+        //! Separate embeds
+        let embed_nothingSelected = new BetterEmbed({
+            interaction, author: { text: "%AUTHOR_NAME | sell", user: interaction.member },
+            description: "Use the reactions to select what you want to sell"
+        });
+
+        //! Collect reactions
+        // Create an array to hold which cards the user wants to sell
+        let cards_toSell = cards_dropped.map(() => null);
+
+        // Create an array of reaction emojis
+        let reactionEmojis = cards_dropped.length > 1 ? [...customEmojis.numbers.slice(0, cards_dropped.length)] : [];
+
+        /// Add the reactions to the message
+        let addReactions = async () => {
+            try {
+                for (let emoji of reactionEmojis) await message.react(emoji.emoji);
+                await message.react(customEmojis.confirmSell.emoji);
+            } catch { }
+        };
+
+        let removeReactions = async () => {
+            try { await message.reactions.removeAll() } catch { }
+        };
+
+        addReactions();
+
+        /// Create the reaction collector        
+        let rc_filter = (reaction, user) =>
+            reactionEmojis.map(emoji => emoji.name).includes(reaction.emoji.name)
+            && user.id === interaction.user.id;
+
+        let rc_collector = message.createReactionCollector({
+            filter: rc_filter, dispose: true,
+            time: dateTools.parseStr(timeout.reactionSell)
+        });
+
+        // Select a card to sell, or confirm the sell
+        rc_collector.on("collect", async reaction => {
+            // Reset the collector's timer
+            rc_collector.resetTimer();
+
+            // Ignore user added reactions
+            if (![...reactionEmojis.map(e => e.name), customEmojis.confirmSell.name].includes(reaction.emoji.name)) return;
+            // Check if the user reacted to a card number
+            let card_idx = reactionEmojis.findIndex(emoji => emoji.name === reaction.emoji.name);
+            if (card_idx !== -1) {
+                // Insert the card at the appropriate index
+                // avoiding adding the same card twice
+                if (!cards_toSell.find(card => card?.uid === cards_dropped[card_idx].uid))
+                    cards_toSell[card_idx] = cards_dropped[card_idx].uid;
+                return;
+            }
+
+            // Remove the sell reactions
+            await removeReactions();
+
+            // Check that there's cards selected
+            if (!cards_toSell.filter(c => c).length) {
+                await messageTools.deleteAfter(
+                    await embed_nothingSelected.send({ method: "followUp" }),
+                    dateTools.parseStr(timeout.errorMessage)
+                );
+
+                return await addReactions();
+            }
+        });
+
+        // Deselect a card on reaction remove
+        rc_collector.on("remove", async reaction => {
+            // Reset the reaction collector's timer
+            rc_collector.resetTimer();
+
+            // Check if the user reacted to a card number
+            let cardIndex = reactionEmojis.findIndex(emoji => emoji.name === reaction.emoji.name);
+
+            // Insert a null at the appropriate index
+            if (cardIndex !== -1) cards_toSell[cardIndex] = null;
+        });
     }
 };
