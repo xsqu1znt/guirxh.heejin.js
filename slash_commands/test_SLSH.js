84caa9ebf24dd2bca598b551e6d66850732bfd69
xsqu1zn't <fluffyspiderstudios@gmail.com>
2023-09-04 16:37:31 -1000

    genius moment

--- slash_commands/test_SLSH.js
+++ slash_commands/test_SLSH.js
@@ -31,38 +31,42 @@ module.exports = {
 		let card_categories = [];
 
 		for (let cat of cardManager.cards.category.names.all) {
+			/// Parse the global IDs found into a format that includes its category name on the first element in the category
 			let _globalIDs = cards.filter(c => c.category === cat).map(c => ({ name: "", globalID: c.globalID }));
 
+			// NOTE: only pushes to the array if there were global IDs found
 			if (_globalIDs.length) {
+				// Gets the first element in the array and appends its category name
 				let _globalID_first = { ..._globalIDs.shift(), name: cat };
+				// Pushes to the main array
 				card_categories.push(...[_globalID_first, ..._globalIDs]);
 			}
 		}
 
 		/// Split global IDs by category, with a max of 5 cards per group
 		let card_categories_split = [];
+		let row_size = 5;
 
 		for (let i = 0; i < card_categories.length;) {
-			let size = 5;
+			// Get the base row size of 5
+			let size = row_size;
+
+			// Get an array of the next 5 entries
 			let chunk_test = card_categories.slice(i, i + size);
-			let chunk_test_idx = chunk_test.findIndex()
+			// Check if it contains an entrie with a category name
+			let chunk_test_idx = chunk_test.findIndex(c => c.name);
 
-			console.log(card_categories[i]);
-			i++;
+			// Cut the row size down to the index of an entry before the category name
+			// we don't need to subtract 1 from here to get the entry before index because we're using .slice() 
+			if (chunk_test_idx > 0) size = chunk_test_idx;
 
-			// card_categories_split.push(chunk);
-			// i += chunk.length;
+			// Push the resulting chunk to the array
+			// since we're using .slice(), we need to make sure the size isn't 0 (the index of .findIndex())
+			card_categories_split.push(chunk_test.slice(0, size || 1));
+
+			// Increment i by size, defaulting to row size if 0 (the index of .findIndex())
+			i += size || row_size;
 		}
-
-		// console.log(card_categories_split);
-
-		// prettier-ignore
-		/* let card_categories = cardManager.cards.category.names.all.map(cat => {
-			let globalIDs = cards.filter(c => c.category === cat).map(c => c.globalID);
-
-			return { name: cat, globalIDs: _jsT.chunk(globalIDs, 5), count: globalIDs.length };
-			
-		}).filter(cat => cat.count); */
 
 		return await embed.send();
 	}
