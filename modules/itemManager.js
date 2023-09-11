e0b0ba76f4ec043061ee8610584006ca5ad4f683
xsqu1zn't <fluffyspiderstudios@gmail.com>
2023-09-11 03:20:17 -1000

    stuff

---
+++ modules/itemManager.js
@@ -0,0 +1,10 @@
+const _jst = require("./jsTools/_jsT");
+
+const items = {
+	boosts: require("../items/boosts.json"),
+	cards: require("../items/card_packs.json")
+};
+
+const config = {
+	bot: require("../configs/config_bot.json")
+};
