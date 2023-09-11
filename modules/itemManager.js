dc5f3f0b270d007dfcae0a76d20cb7de614bf176
xsqu1zn't <fluffyspiderstudios@gmail.com>
2023-09-11 03:57:38 -1000

    item stuff

--- modules/itemManager.js
+++ modules/itemManager.js
@@ -1,10 +1,41 @@
+/** @typedef {"card_pack"|"badge"|"boost"} ItemType */
+
 const _jst = require("./jsTools/_jsT");
 
 const items = {
-	boosts: require("../items/boosts.json"),
-	cards: require("../items/card_packs.json")
+	card_packs: require("../items/card_packs.json"),
+	badges: require("../items/badges.json"),
+	boosts: require("../items/boosts.json")
 };
 
 const config = {
 	bot: require("../configs/config_bot.json")
 };
+
+/** @param {string} id */
+function get(id) {
+	let item = items.card_packs.find(c => c.id === id);
+	let itemType = "card_pack";
+
+	if (!item) {
+		item = items.badges.find(b => b.id === id);
+		itemType = "badge";
+	}
+
+	if (!item) {
+		item = items.boosts.find(b => b.id === id);
+		itemType = "boost";
+	}
+
+	return { item: item ? structuredClone(item) : null, type: item ? itemType : "n/a" };
+}
+
+/** @param {string} id @param {ItemType} itemType */
+function buy(id, itemType) {
+	// prettier-ignore
+	switch (itemType) {
+        case "card_pack": return;
+        case "badge": return;
+        case "boost": return;
+    }
+}
