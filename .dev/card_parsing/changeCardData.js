27d844bc11950334c07e607036f7273c6380232c
xsqu1zn't <fluffyspiderstudios@gmail.com>
2023-09-12 02:12:29 -1000

    card stuff

--- .dev/card_parsing/changeCardData.js
+++ .dev/card_parsing/changeCardData.js
@@ -3,19 +3,19 @@ const fs = require("fs");
 const logger = require("../../modules/logger");
 
 const cards = {
-	// cards_bday: require('../../items/cards/cards_bday.json'),
-	// cards_common: require('../../items/cards/cards_common.json'),
-	// cards_custom: require('../../items/cards/cards_custom.json'),
-	// cards_epic: require('../../items/cards/cards_epic.json'),
+	cards_bday: require('../../items/cards/cards_bday.json'),
+	cards_common: require('../../items/cards/cards_common.json'),
+	cards_custom: require('../../items/cards/cards_custom.json'),
+	cards_epic: require('../../items/cards/cards_epic.json'),
 	cards_event1: require("../../items/cards/cards_event1.json"),
 	cards_event2: require("../../items/cards/cards_event2.json"),
-	cards_event3: require("../../items/cards/cards_event3.json")
-	// cards_holiday: require('../../items/cards/cards_holiday.json'),
-	// cards_mint: require('../../items/cards/cards_mint.json'),
-	// cards_rare: require('../../items/cards/cards_rare.json'),
-	// cards_season: require('../../items/cards/cards_season.json'),
-	// cards_shop: require('../../items/cards/cards_shop.json'),
-	// cards_uncommon: require('../../items/cards/cards_uncommon.json')
+	cards_event3: require("../../items/cards/cards_event3.json"),
+	cards_holiday: require('../../items/cards/cards_holiday.json'),
+	cards_mint: require('../../items/cards/cards_mint.json'),
+	cards_rare: require('../../items/cards/cards_rare.json'),
+	cards_season: require('../../items/cards/cards_season.json'),
+	cards_shop: require('../../items/cards/cards_shop.json'),
+	cards_uncommon: require('../../items/cards/cards_uncommon.json')
 };
 
 let card_keys = Object.keys(cards);
@@ -23,8 +23,13 @@ let card_values = Object.values(cards);
 
 for (let i = 0; i < card_values.length; i++) {
 	card_values[i].forEach((_card, idx) => {
-		if (_card.setID === "199") card_values[i][idx].description = `**Chuu & Yves** \`Special\``;
-		if (_card.setID === "122") card_values[i][idx].description = `**LOONA Units** \`Special\``;
+		let _description = _card.description.split(" `").map(c => c.replace(/`/g, ""));
+		_description[1] = `*\`${_description[1]}\`*`;
+
+		card_values[i][idx].description = _description.join(" ");
+
+		// if (_card.setID === "199") card_values[i][idx].description = `**Chuu & Yves** \`Special\``;
+		// if (_card.setID === "122") card_values[i][idx].description = `**LOONA Units** \`Special\``;
 	});
 
 	// Parse the object into a string
