30cec30f3986628bb820daf8e39739c347b22481
xsqu1zn't <fluffyspiderstudios@gmail.com>
2023-09-15 16:59:51 -1000

    maybe?

--- modules/dropManager.js
+++ modules/dropManager.js
@@ -20,21 +20,45 @@ async function drop(userID, dropType, count) {
 
 	let cards = [];
 
-	const drop_general = () => {
-		let _category = _jsT.choiceWeighted(Object.values(config.drop.chance).map(c => ({ ...c, rarity: c.CHANCE })));
-		let _cards = cardManager.cards.general.filter(card => card.rarity === _category.cardRarityFilter);
+	const drop_general = async () => {
+		/// Randomly pick the cards
+		let cards = [];
 
-		return _jsT.choice(_cards, true);
+		for (let i = 0; i < config.drop.count.general; i++) {
+			let _category = _jsT.choiceWeighted(Object.values(config.drop.chance).map(c => ({ ...c, rarity: c.CHANCE })));
+			let _cards = cardManager.cards.general.filter(card => card.rarity === _category.cardRarityFilter);
+
+			cards.push({ card: _jsT.choice(_cards, true), _gID_pool: _cards.map(c => c.globalID) });
+		}
+
+		if (charm_dupeRepel) {
+			// prettier-ignore
+			// Check if the user already has the chosen cards
+			let _isDupe = await userManager.inventory.has(userID, cards.map(c => c.globalID));
+
+			// Calculate reroll chances
+			let _reroll = _isDupe.map(d => (d ? _jsT.chance(charm_dupeRepel.power) : false));
+
+			// Reroll if needed (checks if there's at least 1 true case)
+			if (_reroll.filter(r => r).length)
+				for (let i = 0; i < _reroll.length; i++)
+					if (_reroll[i]) {
+						// Check which cards the user has in the current category
+						let _has = await userManager.inventory.has(userID, cards[i]._gID_pool);
+						let _possible_gID_pool = cards[i]._gID_pool.filter(gid => !_has.includes(gid));
+
+						// Reroll a new global ID if possible
+						if (_possible_gID_pool.length)
+							cards[i].card = cardManager.get.globalID(_jsT.choice(_possible_gID_pool));
+					}
+		}
+
+		return cards.map(c => c.card);
 	};
 
 	switch (dropType) {
 		// prettier-ignore
-		case "general":
-            for (let i = 0; i < count; i++) {
-                
-            }
-            
-            return;
+		case "general": drop_general;
 
 		// prettier-ignore
 		case "weekly": return;
