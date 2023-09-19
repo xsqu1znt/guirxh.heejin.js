63e8ef56bb498f79db2e7700dca844d592ab4a25
xsqu1zn't <fluffyspiderstudios@gmail.com>
2023-09-18 18:58:18 -1000

    viewing thing

--- slash_commands/view_SLSH_MP.js
+++ slash_commands/view_SLSH_MP.js
@@ -11,11 +11,11 @@ module.exports = {
 
 	// prettier-ignore
 	builder: new SlashCommandBuilder().setName("view")
-        .setDescription("View information about a card")
+		.setDescription("View information about a card")
 
 		.addStringOption(option => option.setName("uid").setDescription("UID of a card you own"))
 		.addStringOption(option => option.setName("gid").setDescription("GID of any card"))
-        .addStringOption(option => option.setName("setid").setDescription("Set ID to view all cards in a set"))
+		.addStringOption(option => option.setName("setid").setDescription("Set ID to view all cards in a set"))
 
 		.addStringOption(option => option.setName("section").setDescription("More sections to view")
 			.addChoices(
@@ -46,27 +46,27 @@ module.exports = {
 			// Fetch the user from Mongo
 			let userData = await userManager.fetch(interaction.user.id, { type: "essential" });
 
-			// Create the view embed
+			// Create the embed :: { VIEW }
 			let embed_view = general_ES.view(interaction.member, userData, card, "uid");
 			return await embed_view.send({ interaction });
 		}
-		
+
 		else if (globalID) {
 			// Get the card from our database
 			let card = cardManager.get.globalID(globalID);
 			if (!card) return await error_ES.send({ interaction, description: "You need to give a valid GID" });
 
-			// Create the view embed
+			// Create the embed :: { VIEW }
 			let embed_view = general_ES.view(interaction.member, null, card, "gid");
 			return await embed_view.send({ interaction });
 		}
-		
+
 		else if (setID) {
 			// Get the cards from our database
 			let cards = cardManager.get.setID(setID);
 			if (!cards.length) return await error_ES.send({ interaction, description: "You need to give a valid set ID" });
 
-			// Create the view embed
+			// Create the embeds :: { VIEW }
 			let embeds_view = general_ES.view(interaction.member, null, cards, "set");
 
 			// prettier-ignore
@@ -80,15 +80,54 @@ module.exports = {
 		}
 
 		else if (section) {
+			await interaction.deferReply();
+
 			// Fetch the user from Mongo
 			let userData = await userManager.fetch(interaction.user.id, { type: "essential" });
 
+			let card = null;
+
 			switch (section) {
-				case "favorite": return;
-				case "idol": return;
+				// Fetch a card from the user's card_inventory :: { FAVORITE }
+				case "favorite":
+					card = await userManager.inventory.get(interaction.user.id, { uids: userData.card_favorite_uid });
+					if (!card) return await error_ES.send({ interaction, description: "You do not have a favorite card\n> Use \`/set\` \`edit:⭐ favorite\` to set one" });
+					break;
+
+				// Fetch a card from the user's card_inventory :: { IDOL }
+				case "idol":
+					card = await userManager.inventory.get(interaction.user.id, { uids: userData.card_selected_uid });
+					if (!card) return await error_ES.send({ interaction, description: "You do not have an idol set\n> Use \`/set\` \`edit:🏃 idol\` to set one" });
+					break;
+
+				// Fetch cards from the user's card_inventory :: { VAULT }
 				case "vault": return;
-				case "team": return;
+
+				// Fetch cards from the user's card_inventory :: { TEAM }
+				case "team":
+					let cards = await userManager.inventory.get(interaction.user.id, { uids: userData.card_team_uids });
+					if (!cards.length) return await error_ES.send({ interaction, description: "You do not have a team set\n> Use \`/set\` \`edit:👯 team\` to set one" });
+
+					// Create the embeds :: { VIEW }
+					let embeds_view = general_ES.view(interaction.member, userData, card, "team");
+
+					// prettier-ignore
+					// Send the embeds with navigation
+					let embedNav = new EmbedNavigator({
+						interaction, embeds: [embeds_view],
+						pagination: { type: "short", dynamic: false, useReactions: true }
+					});
+
+					return await embedNav.send();
 			}
+
+			if (card) {
+				// Create the embed :: { VIEW }
+				let embed_view = general_ES.view(interaction.member, userData, card, section);
+				return await embed_view.send({ interaction });
+			}
+
+			return await error_ES.send({ interaction, description: `\`${section}\` is not a valid option` });
 		}
 	}
 };
