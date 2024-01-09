5353d0dba326542554556fec4e4ad066397de6cb
xsqu1zn't <fluffyspiderstudios@gmail.com>
2024-01-08 17:50:55 -1000

    bug fix

--- modules/embedStyles/index.js
+++ modules/embedStyles/index.js
@@ -12,7 +12,7 @@ const cooldown_ES = new BetterEmbed({ author: { text: "⏳ Cooldown" } });
 /** @param {GuildMember|User} user @param {import("../mongo/uM_cooldowns").CooldownType} reminderType */
 const reminder_ES = (user, reminderType) => new BetterEmbed({
     author: { text: "⏰ Reminder", user },
-    description: `Hey $USER, your \`${jt.toTitleCase(reminderType.replace("_", ""))}\` is **available**!`
+    description: `Hey $USER, your \`${jt.toTitleCase(reminderType.replace("_", " "))}\` is **available**!`
 });
 
 module.exports = { user_ES, general_ES, error_ES, cooldown_ES, reminder_ES };
