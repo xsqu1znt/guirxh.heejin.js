4f788e7b8511106ebfd644c913f89338724a286e
xsqu1zn't <fluffyspiderstudios@gmail.com>
2024-01-12 20:57:39 -1000

    server list

--- slash_commands/admin/admin_SLSH.js
+++ slash_commands/admin/admin_SLSH.js
@@ -11,7 +11,7 @@ const {
 	ComponentType
 } = require("discord.js");
 
-const { BetterEmbed } = require("../../modules/discordTools");
+const { BetterEmbed, EmbedNavigator } = require("../../modules/discordTools");
 const { error_ES } = require("../../modules/embedStyles/index");
 const { userManager } = require("../../modules/mongo/index");
 const cardManager = require("../../modules/cardManager");
@@ -26,6 +26,104 @@ async function subcommand_test(client, interaction) {
 	await interaction.deferReply().catch(() => null);
 
 	throw new Error("hah, gotcha!");
+}
+
+/** @param {Client} client @param {CommandInteraction} interaction */
+async function subcommand_server(client, interaction) {
+	// Interaction options
+	let serverID = interaction.options.getString("server") || null;
+
+	let embed_error = new BetterEmbed({
+		interaction,
+		author: { text: "$USERNAME | servers", iconURL: true },
+		description: "Could not fetch guilds"
+	});
+
+	// Fetch all the guilds the client's currently in
+	let guilds_partial = await client.guilds.fetch();
+
+	//* Try to leave the server if an ID is given
+	if (serverID)
+		try {
+			// Fetch the server based on the given ID
+			let guildToLeave = await guilds_partial.find(guild => guild.id === serverID).fetch();
+
+			// Leave the server
+			await guildToLeave.leave();
+
+			// Log the leave
+			logger.success(`successfully left server (${serverID})`);
+
+			// Let the user know the result
+			return await embed_error.send({ description: `Successfully left server: ${inline(true, "🆔", serverID)}` });
+		} catch (err) {
+			// Log the error
+			logger.error("Could not leave server", `server id: ${serverID}`, err);
+
+			// Let the user know that the server couldn't be left
+			return await embed_error.send({ description: `Failed to leave server: ${inline(true, "🆔", serverID)}` });
+		}
+
+	//* Show the servers the client's currently in
+	// Fetch each guild to get the full guild object
+	let guilds_full = await Promise.all(guilds_partial.map(guild => guild.fetch()));
+
+	// Sort by join date
+	guilds_full = guilds_full.sort((a, b) => a.joinedTimestamp - b.joinedTimestamp);
+
+	// Parse each guild into a human readable string
+	let guilds_full_f = await Promise.all(
+		guilds_full.map(async (guild, idx) => {
+			let invite_url = "";
+
+			/* try {
+				// Fetch the inviteManager from the guild
+				// only works if the client has the Manage Server permission in that guild
+				let inviteManager = await guild.invites.fetch();
+
+				// Get the first active invite in the guild
+				let invite = inviteManager.at(0);
+
+				// Create a new temp invite if an invite doesn't already exist | expires in 5 minutes
+				invite ||= await guild.invites.create(guild.systemChannelId, {
+					maxAge: 60 * 5,
+					maxUses: 1
+				});
+
+				// Get the invite url
+				if (invite) invite_url = invite.url;
+			} catch (err) {
+				// Log the error
+				// logger.error("Could not create server invite", `server id: ${serverID}`, err);
+			} */
+
+			// Return a formatted guild string
+			return "$IDX $GUILD_NAME ・ $GUILD_ID\n> $MEMBER_COUNT : `📆` $JOINED"
+				.replace("$IDX", inline(`${idx + 1}.`))
+				.replace("$GUILD_NAME", bold(invite_url ? link(guild.name, invite_url) : guild.name))
+				.replace("$GUILD_ID", inline("🆔", guild.id))
+				.replace("$MEMBER_COUNT", inline("👥", guild.memberCount))
+				.replace("$JOINED", `<t:${numberTools.milliToSeconds(guild.members.me.joinedTimestamp)}>`);
+		})
+	);
+
+	// Break up the formatted servers to only show 10 per page
+	guilds_full_f = arrayTools.chunk(guilds_full_f, 10);
+
+	// Create a page for each chunk
+	let embeds_servers = [];
+	for (let chunk of guilds_full_f) {
+		let _embed = new BetterEmbed({
+			author: { text: "$USERNAME | servers", iconURL: true },
+			description: chunk.join("\n")
+		});
+
+		embeds_servers.push(_embed);
+	}
+
+	// Send the embed with navigation
+	let embedNav = new EmbedNavigator({ interaction, embeds: [embeds_servers], pagination: { type: "shortJump" } });
+	return await embedNav.send();
 }
 
 /** @param {Client} client @param {CommandInteraction} interaction */
@@ -457,7 +555,7 @@ module.exports = {
         .addStringOption(option => option.setName("command").setDescription("The command you want to use")
             .setRequired(true)
             .addChoices(
-                // { name: "💻 server", value: "server" },
+                { name: "💻 server", value: "server" },
                 { name: "💭 test", value: "test" },
                 { name: "🪶 summon", value: "summon" },
                 { name: "🥕 pay", value: "pay_carrot" },
@@ -468,6 +566,7 @@ module.exports = {
 
         .addUserOption(options => options.setName("user").setDescription("The user"))
         .addNumberOption(options => options.setName("amount").setDescription("Amount to pay (use negative to withdraw)"))
+		.addStringOption(options => options.setName("server").setDescription("ID of the server to **LEAVE**"))
 		.addStringOption(options => options.setName("gid").setDescription("GID of the card (separate by comma)"))
 		.addStringOption(options => options.setName("uid").setDescription("UID of the card")),
 
@@ -478,6 +577,7 @@ module.exports = {
 		// prettier-ignore
 		switch (command) {
             case "test": return await subcommand_test(client, interaction);
+            case "server": return await subcommand_server(client, interaction);
             case "summon": return await subcommand_summon(client, interaction);
             case "pay_carrot": return await subcommand_payUser(interaction, "balance");
             case "pay_ribbon": return await subcommand_payUser(interaction, "ribbon");
